13a368177a2cec3728beae94f346ead15aa58a0e
Krzysztof Piekarz <kpiekarz1983@gmail.com>
2018-04-18 19:41:33 +0200

    Finished 9.4

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function drawTree (levels) {
+	
+	for (var i = 0; i < levels; i++) {
+		
+		var star = '';
+
+		for (var j = 0; j <= i; j++) {
+			star += '*';
+		}
+
+		console.log(star);
+
+	}
+
+}
+
